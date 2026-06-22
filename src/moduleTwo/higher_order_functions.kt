38b58183146aebd5a5f7445866107308eb99b76c
Mikhail Gubanov <gubanov.mihail7@wb.ru>
2026-06-23 00:17:16 +0200

    Module 2 higher order functions
    Added new tasks

--- src/moduleTwo/higher_order_functions.kt
+++ src/moduleTwo/higher_order_functions.kt
@@ -63,24 +63,93 @@ fun processString(s: String, convert: (String)-> String) : String  {
     return convert(s)
 }
 
+/*
+Задание 6
+Принимает: два числа (Int, Int) и операцию (Int, Int) -> Int
+Делает: применяет операцию к двум числам
+Возвращает: Int (результат операции)
+Вызови со сложением
+ */
+
+fun calculate(x: Int, z: Int, op: (Int, Int) -> Int) : Int {
+    return op(x,z)
+}
+
+/*
+Задание 7
+Принимает: одно число (Int) и операцию (Int) -> Int
+Делает: применяет операцию к числу
+Возвращает: Int
+Вызови с операцией "удвоить"
+ */
+
+fun applyTo(x: Int, operate: (Int) -> Int) : Int = operate(x)
+
+/*
+Задание 8
+Принимает: число (Int) и условие (Int) -> Boolean
+Делает: проверяет число условием
+Возвращает: Boolean
+Вызови с проверкой "больше 10"
+ */
+
+fun checkInt(x: Int, op: (Int) -> Boolean) : Boolean = op(x)
+
+/*
+Задание 9
+doTwice
+Принимает: действие () -> Unit
+Делает: выполняет это действие два раза
+Возвращает: ничего (Unit)
+Вызови с печатью текста
+ */
+
+fun doTwice(ln: () -> Unit){
+    for (i in 1..2) ln()
+}
+
+/*
+Задание 10
+transform
+Принимает: строку (String) и операцию (String) -> String
+Делает: применяет операцию к строке
+Возвращает: String
+Вызови с операцией "в верхний регистр" (it.uppercase())
+ */
+
+fun transformToUpperCase(s: String, operate: (String) -> String): String = operate(s)
 
 fun main(){
-    val add = operate(3,4,{ x, y -> x + y })
+    val add = operate(3,4) { x, y -> x + y }
     println(add)
 
-    val multiply = operate(3,4,{ x, y -> x * y })
+    val multiply = operate(3,4) { x, y -> x * y }
     println(multiply)
 
     repeatAction(7, {println("Hello")})
-    val result = transformList(listOf(1,2,3,4,5), {it + 1})
+    val result = transformList(listOf(1,2,3,4,5)) {it + 1}
     println(result)
 
-    val r = filterList(listOf(1,2,3,4,5), {it > 1})
+    val r = filterList(listOf(1,2,3,4,5)) {it > 1}
     println(r)
 
-    var transformString = processString("Stan", {it.reversed()})
+    var transformString = processString("Stan") {it.reversed()}
     println(transformString)
 
-    transformString = processString("stan", {it.replaceFirstChar { c -> c.uppercase() }})
+    transformString = processString("stan") {it.replaceFirstChar { c -> c.uppercase() }}
     println(transformString)
+
+    val opSum = operate(3,4) { x, y -> x + y }
+    println(opSum)
+
+    val addTwo = applyTo(2){it + 2}
+    println(addTwo)
+
+    val hitherThanTen = checkInt(9) {it > 10}
+    println(hitherThanTen)
+
+    doTwice { println("doTwice") }
+
+    val transformation = transformToUpperCase("string") {it.uppercase()}
+    println(transformation)
 }
